7934d347fc808be1e83d61e0144db9147a82d452
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-11-04 17:33:58 -0500

    removed solution

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,31 +4,11 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    index_page = Nokogiri::HTML(open(index_url))
-    students = []
-    index_page.css("div.roster-cards-container").each do |card|
-      card.css(".card-text-container").each do |container|
-        student_name = container.css(".student-name").text
-        student_location = container.css(".student-location").text
-        students << {name: student_name, location: student_location}
-      end
-    end
-    students
+    
   end
 
   def self.scrape_profile_page(profile_url)
-    profile_page = Nokogiri::HTML(open(profile_url))
-    student = {}
     
-    student[:twitter] = profile_page.css(".social-icon-container").children.css("a")[0].attribute("href").value
-    student[:linkedin] = profile_page.css(".social-icon-container").children.css("a")[1].attribute("href").value
-    student[:github] = profile_page.css(".social-icon-container").children.css("a")[2].attribute("href").value
-    student[:blog] = profile_page.css(".social-icon-container").children.css("a")[3].attribute("href").value
-
-    student[:profile_quote] = profile_page.css(".profile-quote").text
-    student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text
-    
-    student
   end
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -5,27 +5,19 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_hash.each do |attribute, value|
-      self.send("#{attribute}=", value)
-    end
-    @@all << self
+    
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |student_hash|
-      Student.new(student_hash)
-    end
+    
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |attr, value|
-      self.send("#{attr}=", value)
-    end
-    self
+    
   end
 
   def self.all
-    @@all
+    
   end
 end
 

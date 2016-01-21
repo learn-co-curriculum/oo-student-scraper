d8bb380a796cb6c85ddefdcb632d5337b6044025
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-01-21 18:12:23 -0500

    removed VCR, changed URL in test suite, updated scraper class to use new URL we switched to using jekyll to store and serve working copy of student site. students will run jekyll serve and scrape that site, instead of students.learn.co which may change or break. updated reamde to explain this and updated tests to use localhost:4000

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ class Scraper
     students = []
     index_page.css("div.roster-cards-container").each do |card|
       card.css(".student-card a").each do |student|
-        student_profile_link = "http://students.learn.co/#{student.attr('href')}"
+        student_profile_link = "http://127.0.0.1:4000/#{student.attr('href')}"
         student_location = student.css('.student-location').text
         student_name = student.css('.student-name').text
         students << {name: student_name, location: student_location, profile_url: student_profile_link}
@@ -18,18 +18,27 @@ class Scraper
 
   def self.scrape_profile_page(profile_slug)
     student = {}
-    begin 
-      profile_page = Nokogiri::HTML(open(profile_slug))
-      student[:twitter] = profile_page.css(".social-icon-container").children.css("a")[0].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[0]
-      student[:linkedin] = profile_page.css(".social-icon-container").children.css("a")[1].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[1]
-      student[:github] = profile_page.css(".social-icon-container").children.css("a")[2].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[2]
-      student[:blog] = profile_page.css(".social-icon-container").children.css("a")[3].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[3]
-      student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
-      student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
-    
-    rescue Exception => e
-      puts e.message
-    end    
+    profile_page = Nokogiri::HTML(open(profile_slug))
+    links = profile_page.css(".social-icon-container").children.css("a").map { |el| el.attribute('href').value}
+    links.each do |link|
+      if link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      elsif link.include?("twitter")
+        student[:twitter] = link
+      else
+        student[:blog] = link
+      end
+    end
+    # student[:twitter] = profile_page.css(".social-icon-container").children.css("a")[0].attribute("href").value 
+    # # if profile_page.css(".social-icon-container").children.css("a")[0]
+    # student[:linkedin] = profile_page.css(".social-icon-container").children.css("a")[1].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[1]
+    # student[:github] = profile_page.css(".social-icon-container").children.css("a")[2].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[2]
+    # student[:blog] = profile_page.css(".social-icon-container").children.css("a")[3].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[3]
+    student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
+    student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
+      
     student
   end
 

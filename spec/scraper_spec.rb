2471e0bec4495af7a9f4f21b8976bdf655fa4180
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-01-21 18:12:23 -0500

    removed VCR, changed URL in test suite, updated scraper class to use new URL we switched to using jekyll to store and serve working copy of student site. students will run jekyll serve and scrape that site, instead of students.learn.co which may change or break. updated reamde to explain this and updated tests to use localhost:4000

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -2,9 +2,9 @@ require "spec_helper"
 
 describe "Scraper" do
 
-  let!(:student_index_array) {[{:name=>"Joe Burgess", :location=>"New York, NY", :profile_url=>"http://students.learn.co/students/joe-burgess.html"},
-                               {:name=>"Mathieu Balez", :location=>"New York, NY", :profile_url=>"http://students.learn.co/students/mathieu-balez.html"},
-                               {:name=>"Diane Vu", :location=>"New York, NY", :profile_url=>"http://students.learn.co/students/diane-vu.html"}]}
+  let!(:student_index_array) {[{:name=>"Joe Burgess", :location=>"New York, NY", :profile_url=>"http://127.0.0.1:4000/students/joe-burgess.html"},
+                               {:name=>"Mathieu Balez", :location=>"New York, NY", :profile_url=>"http://127.0.0.1:4000/students/mathieu-balez.html"},
+                               {:name=>"Diane Vu", :location=>"New York, NY", :profile_url=>"http://127.0.0.1:4000/students/diane-vu.html"}]}
 
   let!(:student_joe_hash) {{:twitter=>"https://twitter.com/jmburges",
                             :linkedin=>"https://www.linkedin.com/in/jmburges",
@@ -14,43 +14,37 @@ describe "Scraper" do
                             :bio=>
   "I grew up outside of the Washington DC (NoVA!) and went to college at Carnegie Mellon University in Pittsburgh. After college, I worked as an Oracle consultant for IBM for a bit and now I teach here at The Flatiron School."}}
 
-  let!(:student_mat_balez) {{:twitter=>"http://twitter.com/matbalez",
-                             :linkedin=>"http://www.linkedin.com/matbalez",
-                             :github=>"http://www.github.com/matbalez",
-                             :profile_quote=>"\"Who dares wins\" - British Special Air Service",
-                             :bio=>
-  "I was born in Northern Ontario where I grew up riding snowmobiles."}}
+  let!(:student_david_hash) {{:linkedin=>"https://www.linkedin.com/in/david-kim-38221690",
+ :github=>"https://github.com/davdkm",
+ :profile_quote=>
+  "\"Yeah, well, you know, that's just, like, your opinion, man.\" - The Dude",
+ :bio=>
+  "I'm a southern California native seeking to find work as a full stack web developer. I enjoying tinkering with computers and learning new things!"}}
 
   describe "#scrape_index_page" do 
     it "is a class method that scrapes the student index page and a returns an array of hashes in which each hash represents one student" do 
-      VCR.use_cassette('fixtures/index_page') do
-        index_url = "http://students.learn.co"
-        scraped_students = Scraper.scrape_index_page(index_url)
-        expect(scraped_students).to be_a(Array)
-        expect(scraped_students.first).to have_key(:location)
-        expect(scraped_students.first).to have_key(:name)
-        expect(scraped_students).to match(student_index_array)
-      end
+      index_url = "http://127.0.0.1:4000/"
+      scraped_students = Scraper.scrape_index_page(index_url)
+      expect(scraped_students).to be_a(Array)
+      expect(scraped_students.first).to have_key(:location)
+      expect(scraped_students.first).to have_key(:name)
+      expect(scraped_students).to include(student_index_array[0], student_index_array[1], student_index_array[2])
     end
   end
 
   describe "#scrape_profile_page" do 
     it "is a class method that scrapes a student's profile page and returns a hash of attributes describing an individual student" do 
-      VCR.use_cassette('fixtures/joe-burgess-profile') do
-        profile_url = "http://students.learn.co/students/joe-burgess.html"
-        scraped_student = Scraper.scrape_profile_page(profile_url)
-        expect(scraped_student).to be_a(Hash)
-        expect(scraped_student).to match(scraped_student)
-      end
+      profile_url = "http://127.0.0.1:4000/students/joe-burgess.html"
+      scraped_student = Scraper.scrape_profile_page(profile_url)
+      expect(scraped_student).to be_a(Hash)
+      expect(scraped_student).to match(student_joe_hash)
     end
 
     it "can handle profile pages without all of the social links" do 
-      VCR.use_cassette('fixtures/mat-balez-profile') do
-        profile_url = "http://students.learn.co/students/mathieu-balez.html"
-        scraped_student = Scraper.scrape_profile_page(profile_url)
-        expect(scraped_student).to be_a(Hash)
-        expect(scraped_student).to match(scraped_student)
-      end
+      profile_url = "http://127.0.0.1:4000/students/david-kim.html"
+      scraped_student = Scraper.scrape_profile_page(profile_url)
+      expect(scraped_student).to be_a(Hash)
+      expect(scraped_student).to match(student_david_hash)
     end
   end
 end

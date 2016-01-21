9cf028629d9a3dd81e23300d8eb621bd8a99bdeb
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-01-21 12:32:40 -0500

    added missing attr_accessor to student class, rescue 404 student profile pages in scraper class

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,6 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 

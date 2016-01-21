d18ac9386113ea2817557e40ff56f5aa387330e1
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-01-21 12:32:40 -0500

    added missing attr_accessor to student class, rescue 404 student profile pages in scraper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,16 +17,19 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_slug)
-    profile_page = Nokogiri::HTML(open(profile_slug))
     student = {}
+    begin 
+      profile_page = Nokogiri::HTML(open(profile_slug))
+      student[:twitter] = profile_page.css(".social-icon-container").children.css("a")[0].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[0]
+      student[:linkedin] = profile_page.css(".social-icon-container").children.css("a")[1].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[1]
+      student[:github] = profile_page.css(".social-icon-container").children.css("a")[2].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[2]
+      student[:blog] = profile_page.css(".social-icon-container").children.css("a")[3].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[3]
+      student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
+      student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
     
-    student[:twitter] = profile_page.css(".social-icon-container").children.css("a")[0].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[0]
-    student[:linkedin] = profile_page.css(".social-icon-container").children.css("a")[1].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[1]
-    student[:github] = profile_page.css(".social-icon-container").children.css("a")[2].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[2]
-    student[:blog] = profile_page.css(".social-icon-container").children.css("a")[3].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[3]
-    student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
-    student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
-    
+    rescue Exception => e
+      puts e.message
+    end    
     student
   end
 

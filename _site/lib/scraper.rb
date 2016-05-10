8a1d808a51cc223bce25eb86c28ab0673793fd18
mendelB <mendelblesofsky@gmail.com>
2016-05-10 10:20:49 -0400

    remove imgs css and html and student pages from everyone besides for the tested student profiles

---
+++ _site/lib/scraper.rb
@@ -0,0 +1,15 @@
+require 'open-uri'
+require 'pry'
+
+class Scraper
+
+  def self.scrape_index_page(index_url)
+    
+  end
+
+  def self.scrape_profile_page(profile_url)
+    
+  end
+
+end
+
---
+++ _site/lib/student.rb
@@ -0,0 +1,23 @@
+class Student
+
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+
+  @@all = []
+
+  def initialize(student_hash)
+    
+  end
+
+  def self.create_from_collection(students_array)
+    
+  end
+
+  def add_student_attributes(attributes_hash)
+    
+  end
+
+  def self.all
+    
+  end
+end
+

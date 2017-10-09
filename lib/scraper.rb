10f366475034beb8fc496fad687ef049349b15e2
Dakota Martinez <dakotaleemusic@gmail.com>
2017-10-09 09:44:39 -0700

    restructure directory so httpserver and tests work better

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,15 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    a = []
+    doc.css("div.student-card a").collect.each_with_index do |student, index|
+      a << {:name => "#{student.css("h4.student-name")[0].text}",
+      :location => "#{student.css("p.student-location")[0].text}",
+      :profile_url => "#{student.attr('href')}" }
+      
+    end
+    a
   end
 
   def self.scrape_profile_page(profile_url)

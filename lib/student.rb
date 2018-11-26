cf5b3c61bf159a435a28d3c3f00e0a45d939b00e
Steven G. Harms <steven.harms@flatironschool.com>
2018-11-26 12:28:56 -0500

    Update Student to use Scraper
    Per spec, `Student` should use `Scraper`

--- lib/student.rb
+++ lib/student.rb
@@ -12,6 +12,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
+    students_array = Scraper.scrape_index_page("./fixtures/student-site/index.html")
     students_array.each do |student_hash|
       Student.new(student_hash)
     end

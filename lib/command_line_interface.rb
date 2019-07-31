46c6b03b2d726c92d093f442cf84c8fa799c9b37
maxwellbenton <maxwellbenton@gmail.com>
2019-07-31 18:37:42 -0400

    fixes typo in base url

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -4,7 +4,7 @@ require 'nokogiri'
 require 'colorize'
 
 class CommandLineInterface
-  BASE_PATH = "https://learn-co-curriculum.github.io/student-scraper-test-page//"
+  BASE_PATH = "https://learn-co-curriculum.github.io/student-scraper-test-page/"
 
   def run
     make_students

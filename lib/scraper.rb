2d641e6090634c3e7220b0cf2f9557f4051160c2
mengshalitsofi <mengshalitsofi@gmail.com>
2021-02-06 16:08:47 -0800

    commit tree 88f09d004e1406e765b800c5df8d526ca5dd45c0

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,54 @@
-require 'open-uri'
+require 'net/http'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    uri = URI.parse(index_url)
+    response = Net::HTTP.get_response(uri)    
+    html = Nokogiri::HTML(response.body)
+
+    students = []
+    html.css("div.student-card").each do |card|
+      student = {}
+      student[:name] = card.css("h4")[0].text
+      student[:location] = card.css("p")[0].text
+      student[:profile_url] = card.css("a").attribute("href").value
+      students << student
+    end
+
+    students
   end
 
   def self.scrape_profile_page(profile_url)
+    profile = {}
+
+    uri = URI.parse(profile_url)
+    response = Net::HTTP.get_response(uri)    
+    html = Nokogiri::HTML(response.body)
+
+    socials = html.css("div.social-icon-container")[0]
     
+    socials.css("a").each do |link|
+      href = link.attribute("href").value
+      if href.include?("twitter")
+        profile[:twitter] = href
+      elsif href.include?("linkedin")
+        profile[:linkedin] = href
+      elsif href.include?("github")
+        profile[:github] = href
+      elsif href.include?("youtube")
+        profile[:youtube] = href
+      else
+        profile[:blog] = href
+      end
+    end
+
+    profile[:profile_quote] = html.css("div.profile-quote")[0].text
+    profile[:bio] = html.css("div.bio-content")[0].css("p")[0].text unless  html.css("div.bio-content")[0].css("p")[0] == nil
+
+    profile
   end
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,26 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    add_student_attributes(student_hash)
+
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key, value|
+      self.send("#{key}=", value) if self.respond_to?("#{key}=")      
+    end
+    self
   end
 
   def self.all
-    
+    @@all
   end
 end
 
